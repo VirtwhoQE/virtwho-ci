783a1c15bb125a9cf1fface43d4f6ffc93906d36
hkx303 <kuhuang@redhat.com>
2020-06-19 17:02:45 +0800

    skip libvirt-local and vdsm mode

--- tests/tier2/tc_2063_check_hypervisors_fqdn.py
+++ tests/tier2/tc_2063_check_hypervisors_fqdn.py
@@ -8,6 +8,9 @@ from virt_who.testing import Testing
 class Testcase(Testing):
     def test_run(self):
         self.vw_case_info(os.path.basename(__file__), case_id='RHEL-188359')
+        hypervisor_type = self.get_config('hypervisor_type')
+        if hypervisor_type in ('libvirt-local', 'vdsm'):
+            self.vw_case_skip(hypervisor_type)
         self.vw_case_init()
 
         # Case Config
